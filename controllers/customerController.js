cf0b99b5463853290a98c8e3e12d5fa0caf56f77
Pawlos Gelgelo <pawlosgelgelo@gmail.com>
2025-10-20 16:58:05 +0300

    Initial commit

---
+++ controllers/customerController.js
@@ -0,0 +1,6 @@
+import { Customer } from "../models/CustomerModel"
+
+const registration = async (req, res) => {
+    const regForm = 
+  Customer.create()
+}
---
+++ models/AdminModel.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const adminSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+export const admin = mongoose.model("Admin", adminSchema)
